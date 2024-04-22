5aba8f303a856afafc407d893ac8c0940c4cf89b
PanBard <93971955+PanBard@users.noreply.github.com>
2024-04-22 17:23:34 +0200

    Add files via upload

---
+++ api/Services/IMyService.cs
@@ -0,0 +1,11 @@
+﻿namespace recognition_api.Services
+{
+    public interface IMyService
+    {
+        void ConnectToQueque();
+        //Task<string> ReceiveMessageAsync();
+        //string ReceiveMessageAsync();
+        Task<string> ReceiveMessageAsyncWithoutMemoryLeakage();
+        void SendImageToQueque(string image);
+    }
+}
