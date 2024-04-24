017dfebc6fce09c0eb4f3fa2b77c47db3fdf92b0
unknown <PanBard>
2024-04-24 02:15:31 +0200

    2

--- recognition_api/Services/IMyService.cs
+++ recognition_api/Services/IMyService.cs
@@ -1,10 +1,20 @@
-﻿namespace recognition_api.Services
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace recognition_api.Services
 {
     public interface IMyService
     {
+        string CompareFaces(double[] embargo);
         void ConnectToQueque();
+        string DeleteDataFromDatabase(int id);
+        string GetDataFromPostgreSQL();
+        (List<string>, List<double[]>) GetSpecificFromPostgreSQL();
+
         //Task<string> ReceiveMessageAsync();
-        string ReceiveMessageAsync();
+        //string ReceiveMessageAsync();
+        Task<dynamic> ReceiveMessageAsyncWithoutMemoryLeakage();
+        string SendDataToDatabase(string name, string password, string imageBase64, dynamic encoding);
+        string SendImageToDB(string image);
         void SendImageToQueque(string image);
     }
 }
